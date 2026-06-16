54e624359af2004f73eb2fe6bca2c559874d3f31
Charley Wu <akunzai@users.noreply.github.com>
2026-06-16 04:21:45 +0000

    gistui 0.10.0

--- Formula/gistui.rb
+++ Formula/gistui.rb
@@ -1,8 +1,8 @@
 class Gistui < Formula
   desc "Terminal UI for managing GitHub Gists"
   homepage "https://akunzai.github.io/gistui/"
-  url "https://github.com/akunzai/gistui/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "75fd557b7fadf6a8dfdeac884d954197529959689b21beffd691bec87ae7a99f"
+  url "https://github.com/akunzai/gistui/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "0be5056ad68aa35d06c2caed7c99dfc6ba7d780ae3073b30fc51d2fabaafe649"
   license "MIT"
   head "https://github.com/akunzai/gistui.git", branch: "main"
 
